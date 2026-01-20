a16a7309c48c52780e3b35df336abbae764c4571
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-01-20 09:29:13 +0530

    Initial commit

---
+++ src/test/java/org/Icet/springtask/SpringtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Icet.springtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
